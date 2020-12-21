bb5d943d8d2babe773c3fdb9c5ab2be2fe6a92fe
Dj Meechlouch <Meechlouch@gmail.com>
2020-12-20 21:32:15 -0600

    connection to mysql declared

--- server.js
+++ server.js
@@ -1,3 +1,17 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql");
 const cTable = require("console.table");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "mysql",
+  database: "employee_tracker_db",
+});
