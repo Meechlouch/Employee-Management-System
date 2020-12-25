f568d8bb163422554988b34097178559ec2a92d6
Dj Meechlouch <Meechlouch@gmail.com>
2020-12-24 23:14:48 -0600

    updated updateEmployee function

--- server.js
+++ server.js
@@ -140,46 +140,52 @@ function addDept() {
 }
 
 function addRole() {
-  inquirer
-    .prompt([
-      {
-        type: "input",
-        name: "addedRole",
-        message: "What is the name of the role you would like to add?",
-      },
-      {
-        type: "number",
-        name: "salary",
-        message: "How much is the yearly salary for this new role?",
-      },
-      {
-        type: "number",
-        name: "roleDeptID",
-        message: "What is the department id number for this new role?",
-      },
-    ])
-    .then((answer) => {
-      console.log(answer);
-      connection.query(
-        "INSERT INTO role SET ?",
-        {
-          title: answer.addedRole,
-          salary: answer.salary,
-          department_id: answer.roleDeptID,
-        },
-        function (err) {
-          if (err) {
-            console.log("You must create a New Department before you create a New Role!");
-            startInquire();
-          }
-          connection.query(`SELECT * FROM role;`, (err, res) => {
-            if (err) throw err;
-            console.table(res);
-            startInquire();
-          });
-        }
-      );
-    });
+  connection.query(`SELECT * FROM department;`, (err, res) => {
+    if (err) throw err;
+    console.table(res);
+  }),
+    setTimeout(() => {
+      inquirer
+        .prompt([
+          {
+            type: "input",
+            name: "addedRole",
+            message: "What is the name of the role you would like to add?",
+          },
+          {
+            type: "number",
+            name: "salary",
+            message: "How much is the yearly salary for this new role?",
+          },
+          {
+            type: "number",
+            name: "roleDeptID",
+            message: "What is the department id number for this new role?",
+          },
+        ])
+        .then((answer) => {
+          console.log(answer);
+          connection.query(
+            "INSERT INTO role SET ?",
+            {
+              title: answer.addedRole,
+              salary: answer.salary,
+              department_id: answer.roleDeptID,
+            },
+            function (err) {
+              if (err) {
+                console.log("You must create a New Department before you create a New Role!");
+                startInquire();
+              }
+              connection.query(`SELECT * FROM role;`, (err, res) => {
+                if (err) throw err;
+                console.table(res);
+                startInquire();
+              });
+            }
+          );
+        });
+    }, 1000);
 }
 
 function addEmployee() {
@@ -296,11 +302,14 @@ function allEmployed() {
 
 //NEEDS TO UPDATE CODE TO SEE ROLE.ID.
 function updateEmployee() {
-  let query = `SELECT *
-                FROM role
-                LEFT JOIN employee
-                ON role.id = role_id;`;
-  connection.query(query, (err, res) => {
+  let query = `SELECT employee.id AS 'ID#', CONCAT(employee.first_name, " ", employee.last_name) AS 'Employees',
+	role.id AS 'Role ID#', role.title AS 'Title', role.salary AS 'Salary', 
+    department_id AS 'Dept ID#', department.dept_name AS 'Department',
+    CONCAT(e.first_name, ' ', e.last_name) AS 'Manager' 
+    FROM role LEFT JOIN employee ON employee.role_id = role.id 
+    INNER JOIN department ON department.id = role.department_id 
+    LEFT JOIN employee e ON employee.manager_id = e.id;`;
+  connection.query(query, function (err, res) {
     if (err) throw err;
     console.table(res);
   });
@@ -308,6 +317,12 @@ function updateEmployee() {
   setTimeout(() => {
     inquirer
       .prompt([
+        {
+          type: "list",
+          name: "toManager",
+          message: "Will Employee be upgraded to a Manager Position?",
+          choices: ["Yes", "No"],
+        },
         {
           type: "number",
           name: "id",
@@ -318,15 +333,36 @@ function updateEmployee() {
           name: "roleID",
           message: "Please enter the new Role ID Number for this Employee",
         },
+        {
+          type: "number",
+          name: "addNull",
+          message: "What is the Manager's ID Number?",
+          when: (answer) => {
+            return answer.toManager === "No";
+          },
+        },
+        {
+          type: "input",
+          name: "addNull",
+          message: "Please type NULL as written!",
+          when: (answer) => {
+            return answer.toManager === "Yes";
+          },
+        },
       ])
       .then((answer) => {
-        let query = `UPDATE employee SET role_id = ${answer.roleID}
-        WHERE id = ${answer.id};`;
-        connection.query(query, (err, res) => {
+        let query = `UPDATE employee SET role_id = ${answer.roleID},
+                     manager_id = ${answer.addNull}
+                     WHERE id = ${answer.id};`;
+        connection.query(query, (err) => {
           if (err) throw err;
-          console.table(res);
-          startInquire();
+          connection.query(`SELECT * FROM employee;`, (err, res) => {
+            console.table(res);
+          });
         });
+        setTimeout(() => {
+          startInquire();
+        }, 1000);
       });
   }, 1000);
 }
